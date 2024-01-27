58f5df863dd68fb8593a056b644953e18ffd0756
m0nkene <157753157+m0nkene@users.noreply.github.com>
2024-01-26 21:14:11 -0500

    further reserve work

--- js/selection.js
+++ js/selection.js
@@ -3,6 +3,8 @@ function generateMap(name){
   return new Promise((resolve,reject) =>{
     sessionStorage.setItem('spawn_name' , name);
 
+    let num = '1';
+
     //reading JSON and determining which map to present - will only use 5 for right now
     fetch('locations/' + name + '/' +  name + '_spawns.json')
       .then(response => response.json())
@@ -10,10 +12,10 @@ function generateMap(name){
         let len = data.factory.length;
         console.log(len);
         let rand = (Math.floor(Math.random() * len)).toString();
-        sessionStorage.setItem('spawn_num' , rand);
+        sessionStorage.setItem('spawn_num' , num);
         console.log(rand);
 
-        let spawn = data.factory.find(spawn => spawn.id === rand);
+        let spawn = data.factory.find(spawn => spawn.id === num);
         console.log(JSON.stringify(spawn));
         sessionStorage.setItem('spawn_data' , JSON.stringify(spawn));
         resolve();
